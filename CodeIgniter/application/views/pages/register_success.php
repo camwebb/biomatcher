82a5c176efcc5b989401dd6bc2afa36095bdca5b
Kartika Dwintaputri S <dhitatracker@gmail.com>
2013-04-05 00:24:53 +0700

    edit resgister_success pages

--- CodeIgniter/application/views/pages/register_success.php
+++ CodeIgniter/application/views/pages/register_success.php
@@ -1,4 +1,5 @@
 <div id="page_title">
     <h1>Biomatcher</h1>
     <p style="font-size: 2em; text-align: center; margin: 20px 0; position: relative;">Biomatcher : a tool for matching digital images</p>
+    <p style="font-size: 2em; text-align: center; padding: 0 0 15px 0; position: relative;"><a href="<?php echo base_url(); ?>index.php">&lt;&lt; Login</a></p>
 </div>
