b52a86ed47cb01c0f8d90e98c87070840158126a
Fitri Nurjannah <widiw374@gmail.com>
2013-09-16 01:00:56 +0700

    Captcha -4
    design

--- CodeIgniter/application/views/captcha/frame.php
+++ CodeIgniter/application/views/captcha/frame.php
@@ -1,10 +1,10 @@
-    
+<div style="height: 700px; width:725px;">
     
     <div id="match" style="padding: 10px;" ><!-- #match height = 641px -->
     
-    <h2>See images below, and choose Same or Different.</h2>
+    <h2>See images below, and choose Same or Different.</h2><br />
         <div class="clear"></div>
-        <div id="image1">
+        <!--<div id="image1">
             <a href="" style="float: left;">
                 <img style="height: 310px; width:310px;" src="" />
             </a>
@@ -13,36 +13,32 @@
             <a href="" style="float: right;">
                 <img style="height: 310px; width:310px;" src="" />
             </a>
-        </div>
-        <!--<ul style="list-style: none; padding: 15px 0; position: absolute;">
-            <li><img style="height: 310px; width:310px;" src="" /></li>
+        </div>-->
+        <ul style="list-style: none; margin-right: 15px; padding: 0; float: left;">
             <li><img style="height: 310px; width:310px;" src="" /></li>
         </ul>
-        <ul style="list-style: none; padding: 15px 0;">
+        
+        <ul style="list-style: none; margin-right: 15px; padding: 0; float: right;">
             <li><img style="height: 310px; width:310px;" src="" /></li>
-        </ul>-->
+        </ul>
         <div class="clear"></div>
-        <br />
+        
         <div style="text-align: center;">
-            <button class="box-button" id="sameMatch">Same</button>
-            <button class="box-button" id="differentMatch">Different</button>
+            <input type="radio" name="match" value="Same" id="same" /><label for="same">Same</label>
+            <input type="radio" name="match" value="Different" id="different" /><label for="different">Different</label>
         </div>
-        <div>
+        <div align="center">
             <p>
-                <img id="siimage" style="border: 1px solid #000; margin-right: 15px" src="<?php echo base_url().'securimage_files/'; ?>securimage_show.php?sid=<?php echo md5(uniqid()) ?>" alt="CAPTCHA Image" align="left" />
-                <object type="application/x-shockwave-flash" data="<?php echo base_url().'securimage_files/'; ?>securimage_play.swf?bgcol=#ffffff&amp;icon_file=<?php echo base_url().'securimage_files/'; ?>images/audio_icon.png&amp;audio_file=<?php echo base_url().'securimage_files/'; ?>securimage_play.php" height="32" width="32">
-                <param name="movie" value="<?php base_url().'securimage_files/'; ?>securimage_play.swf?bgcol=#ffffff&amp;icon_file=<?php echo base_url().'securimage_files/'; ?>images/audio_icon.png&amp;audio_file=<?php echo base_url().'securimage_files/'; ?>securimage_play.php" />
-                </object>
-                &nbsp;
-                <a tabindex="-1" style="border-style: none;" href="#" title="Refresh Image" onclick="document.getElementById('siimage').src = '<?php echo base_url().'securimage_files/'; ?>securimage_show.php?sid=' + Math.random(); this.blur(); return false"><img src="<?php echo base_url().'securimage_files/'; ?>images/refresh.png" alt="Reload Image" height="32" width="32" onclick="this.blur()" align="bottom" border="0" /></a><br />
-                <strong>Enter Code*:</strong><br />
-                <input type="text" name="ct_captcha" size="12" maxlength="8" />
+                <img id="siimage" style="border: 1px solid #000; margin-right: 15px;" src="<?php echo base_url().'securimage_files/'; ?>securimage_show.php?sid=<?php echo md5(uniqid()) ?>" alt="CAPTCHA Image" />
+                
+                <a tabindex="-1" style="border-style: none;" href="#" title="Refresh Image" onclick="document.getElementById('siimage').src = '<?php echo base_url().'securimage_files/'; ?>securimage_show.php?sid=' + Math.random(); this.blur(); return false"><img src="<?php echo base_url().'securimage_files/'; ?>images/refresh.png" alt="Reload Image" height="32" width="32" onclick="this.blur()" align="top" border="0" /></a><br />
+                <input align="top" class="inputtext-reg" type="text" name="ct_captcha" size="12" maxlength="8" style="float:none; width:150px; height: 37px;" />
+                <button type="submit" class="box-button" id="sameMatch">Submit</button>
+                               
+                
             </p>
-            
-            <p>
-                <br />
-                <input type="submit" value="Submit Message" />
-            </p>
+
         </div>
         
     </div>
+</div>
--- CodeIgniter/application/views/captcha/iframe_captcha_testing.php
+++ CodeIgniter/application/views/captcha/iframe_captcha_testing.php
@@ -7,9 +7,13 @@
     <script type="text/javascript">
     function show_mask(){
         // Add the mask to body
-		$('body').append('<iframe style="height:441px; width:699px;" src="http://192.168.56.10/biomatcher/biomatcher.org/index.php/captcha/frame"></iframe>');
+		$('body').append('<iframe style="height:700px; width:725px;" src="http://192.168.56.10/biomatcher/biomatcher.org/index.php/captcha/frame"></iframe>');
 		$('#mask').fadeIn(300);
     }
+    
+    var RecaptchaOptions = {
+            theme : 'white'
+         };
 
     </script>
 </head>
@@ -18,5 +22,28 @@
     show_mask();
 </script>
 
+
+<form action="" method="post">
+
+    <!-- ... your form code here ... -->
+
+    <script type="text/javascript"
+       src="http://www.google.com/recaptcha/api/challenge?k=6LcqRuYSAAAAAPP6mZvCEUur8j_wVTIDAHPmmsZ-">
+    </script>
+    <noscript>
+       <iframe src="http://www.google.com/recaptcha/api/noscript?k=6LcqRuYSAAAAAPP6mZvCEUur8j_wVTIDAHPmmsZ-"
+           height="300" width="500" frameborder="0"></iframe><br>
+       <textarea name="recaptcha_challenge_field" rows="3" cols="40">
+       </textarea>
+       <input type="hidden" name="recaptcha_response_field"
+           value="manual_challenge">
+    </noscript>
+
 </body>
 </html>
+
+<!-- please read later
+
+http://stackoverflow.com/questions/14098802/how-to-create-embed-code-for-other-sites
+
+-->
