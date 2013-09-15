f7a60af7bf5592761fefcc41b6fe769c75efd86b
Kartika Dwintaputri S <dhitatracker@gmail.com>
2013-09-16 02:19:37 +0700

    Edit Label CSV (duplicated bug)

--- CodeIgniter/application/models/m_pages.php
+++ CodeIgniter/application/models/m_pages.php
@@ -144,6 +144,9 @@ class M_pages extends CI_Model {
         $this->load->library('csvreader');
         $result = $this->csvreader->parse_file($path_csv.'/'.$folder_encrypt.'.csv');
         $this->db->insert_batch('tmp_image', $result);
+        $data = array('label' => NULL);
+        $this->db->where('projectID', $project_address);
+        $this->db->update('image', $data); 
         $query1= $this->db->query("UPDATE image INNER JOIN tmp_image on tmp_image.FILENAME = image.nameOri SET image.label = NULL WHERE image.projectID = $project_address AND tmp_image.LABEL='';");
         $this->db->insert_batch('tmp_image', $result);
         $query2= $this->db->query("UPDATE image INNER JOIN tmp_image on tmp_image.FILENAME = image.nameOri SET image.label = tmp_image.LABEL WHERE image.projectID = $project_address AND tmp_image.LABEL!='';");
