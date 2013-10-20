5aa22ea6b0fc4e144377f0a8f74a2c20111481e2
Kartika Dwintaputri Siregar <dhitatracker@gmail.com>
2013-10-21 01:06:09 +0700

    iframe popup #2

--- CodeIgniter/application/views/captcha/frame.php
+++ CodeIgniter/application/views/captcha/frame.php
@@ -12,42 +12,41 @@ if (isset($_SESSION['ctform']['error']) &&  $_SESSION['ctform']['error'] == true
 <input type="hidden" name="do" value="contact" />
 
 <div class="biomatcher">
-    <div>
         
-        <div id="match" style="padding: 10px;" >
+    <div id="match" style="padding: 10px 25px" >
+    
+    <h2 align="center">See images below, and choose Same or Different.</h2>
+
+    <ul class="biomatcher-ul-image" style="float: left;">
+        <li><img class="biomatcher-image" src="" /></li>
+    </ul>
+    
+    <ul class="biomatcher-ul-image" style="float: right;">
+        <li><img class="biomatcher-image" src="" /></li>
+    </ul>
         
-        <h2 align="center">See images below, and choose Same or Different.</h2>
-            <div class="clear"></div>
-    
-            <ul class="biomatcher-ul-image" style="float: left;">
-                <li><img class="biomatcher-image" src="" /></li>
-            </ul>
-            
-            <ul class="biomatcher-ul-image" style="float: right;">
-                <li><img class="biomatcher-image" src="" /></li>
-            </ul>
-            <div class="clear"></div>
-            
-            <div style="text-align: center;">
-                <input type="radio" name="match" value="Same" id="same" /><label for="same">Same</label>
-                <input type="radio" name="match" value="Different" id="different" /><label for="different">Different</label>
-            </div>
-            <div align="center">
-                <p>
-                    <img id="siimage" style="border: 1px solid #000; margin-right: 15px;" src="<?php echo base_url().'securimage_files/'; ?>securimage_show.php?sid=<?php echo md5(uniqid()) ?>" alt="CAPTCHA Image" />
-                    
-                    <a tabindex="-1" style="border-style: none;" href="#" title="Refresh Image" onclick="document.getElementById('siimage').src = '<?php echo base_url().'securimage_files/'; ?>securimage_show.php?sid=' + Math.random(); this.blur(); return false"><img src="<?php echo base_url().'securimage_files/'; ?>images/refresh.png" alt="Reload Image" height="32" width="32" onclick="this.blur()" align="bottom" border="0" /></a><br />
-                    
-                    <input class="biomatcher-inputtext-reg" type="text" name="ct_captcha" size="12" maxlength="8" />
-                    
-                    
-                    <button type="submit" class="biomatcher-box-button" id="sameMatch">Send Information</button>               
-                </p>
-    
-            </div>
-            
-        </div>
+    <div class="clear"></div>
+        
+    <div style="text-align: center;">
+        <input type="radio" name="match" value="Same" id="same" /><label for="same">Same</label>
+        <input type="radio" name="match" value="Different" id="different" /><label for="different">Different</label>
     </div>
+    
+    <div align="center">
+        <p>
+            <img id="siimage" style="border: 1px solid #000; margin-right: 15px;" src="<?php echo base_url().'securimage_files/'; ?>securimage_show.php?sid=<?php echo md5(uniqid()) ?>" alt="CAPTCHA Image" />
+            
+            <a tabindex="-1" style="border-style: none;" href="#" title="Refresh Image" onclick="document.getElementById('siimage').src = '<?php echo base_url().'securimage_files/'; ?>securimage_show.php?sid=' + Math.random(); this.blur(); return false"><img src="<?php echo base_url().'securimage_files/'; ?>images/refresh.png" alt="Reload Image" height="32" width="32" onclick="this.blur()" align="bottom" border="0" /></a><br />
+            
+            <input class="biomatcher-inputtext-reg" type="text" name="ct_captcha" size="12" maxlength="8" />
+            
+            
+            <button type="submit" class="biomatcher-box-button" id="sameMatch">Send Information</button>               
+        </p>
+
+    </div>       
+    </div>
+    
 </div>
 </form>
 
--- biomatcher.org/captcha/biomatcher.js
+++ biomatcher.org/captcha/biomatcher.js
@@ -1,4 +1,9 @@
 function biomatcher(){
-    $('body').append('<div class="overlay"></div><div id="biomatcher_captcha" class="popup" ><a title="Close" class="popup_close" href="javascript:;"></a></div>');
-    $('#biomatcher_captcha').append('<iframe src="http://biomatcher.org/index.php/captcha//frame" height="630px" width="761px" frameborder="0" ></iframe>');
+    $('body').append('<div class="overlay"></div><div id="biomatcher_captcha" class="popup shadow" ><a title="Close" class="popup_close" onclick="popup_close()"></a></div>');
+    $('#biomatcher_captcha').append('<iframe src="http://localhost/biomatcher-localhost/biomatcher.org/index.php/captcha//frame" height="630px" width="761px" frameborder="0" ></iframe>');
     }
+    
+function popup_close(){
+    $('.overlay').remove();
+    $('#biomatcher_captcha').remove();
+}
