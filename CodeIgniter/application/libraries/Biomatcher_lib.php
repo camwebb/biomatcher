44f82306f3485ded5e221f433b33cb78c8fa04ef
Fitri Nurjannah <widiw374@gmail.com>
2013-10-25 23:02:53 +0700

    Add Library

---
+++ CodeIgniter/application/libraries/Biomatcher_lib.php
@@ -0,0 +1,32 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); 
+
+class Biomatcher_lib {
+    
+    private $CI;
+
+    public function __construct()
+    {
+        $this->CI =& get_instance();
+    }
+
+    public function selectQC(){
+        $this->CI->load->model('m_pages');
+        $result_project = $this->CI->m_pages->selectQCProject();
+        $shuffled_project = array();
+        foreach ($result_project as $project){
+            $projectID = $project->id;
+            //check if project contain min 5 images
+            //note: active project?
+            if($this->CI->m_pages->activeProject($projectID)){
+                $shuffled_project[] = $project;
+            }
+        }
+        //shuffle array from project
+        shuffle ($shuffled_project);
+        
+        return $shuffled_project;
+    
+    }
+}
+
+/* End of file Biomatcher_lib.php */
