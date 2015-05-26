bb9f8008478f8f41448044273f3672a8cb466df7
kartikaDS <dhitatracker@gmail.com>
2015-05-27 01:25:28 +0700

    Demo page #2
    edit register url

--- CodeIgniter/application/views/demo/index.php
+++ CodeIgniter/application/views/demo/index.php
@@ -4,16 +4,16 @@
 	<script type="text/javascript" src="http://biomatcher.org/style/js/jquery-2.0.3.js"></script>
 	<script type="text/javascript" src="http://biomatcher.org/captcha/biomatcher.js"></script>
 	<script type="text/javascript">
-	    window.addEventListener( "message",
-	    function (e) {
-	        if(e.data == 'verified'){
-	            //do something with your form ex. submit
-	            document.forms["myForm"].submit();    
-	        } 
-	    },
-	    false);
-	    var yourURL = 'http://kds.my.id'; //this is used to send a message to your site.
-	    var token = '6f6bf6b5c4e88485024b8f49a202fa5ccc75024062223184ba6fc5cc0af93fea'; //this token will allow you to use biomatcher captcha.
+	    window.addEventListener( "message",
+	    function (e) {
+	        if(e.data == 'verified'){
+	            //do something with your form ex. submit
+	            document.forms["myForm"].submit();    
+	        } 
+	    },
+	    false);
+	    var yourURL = 'http://biomatcher.org/index.php/demo'; //this is used to send a message to your site.
+	    var token = '7e50ff0667c82da61df46f6d824045f7cf78896130cb1f78067de67e425dcabc'; //this token will allow you to use biomatcher captcha.
 	</script>
 </head>
 
