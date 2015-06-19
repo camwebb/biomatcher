f3417eb561ac6ccb50fc98d35412a0133b0f3bdb
kartikaDS <dhitatracker@gmail.com>
2015-06-19 15:49:54 +0700

    Minor style edit

--- CodeIgniter/application/views/setting/index.php
+++ CodeIgniter/application/views/setting/index.php
@@ -19,25 +19,25 @@
             <tr>
                 <td>Name</td>
                 <td>:</td>
-                <td><input class="inputtext-admin" type="text" name="name" value="<?php echo $this->session->userdata('name') ?>"/></td>
+                <td><input class="inputtext-setting" type="text" name="name" value="<?php echo $this->session->userdata('name') ?>"/></td>
                 <td><div id="error-name" class="errorbox"></div></td>
             </tr>
             <tr>
                 <td>Username</td>
                 <td>:</td>
-                <td><input class="inputtext-admin" type="text" name="username" value="<?php echo $this->session->userdata('username') ?>"/></td>
+                <td><input class="inputtext-setting" type="text" name="username" value="<?php echo $this->session->userdata('username') ?>"/></td>
                 <td><div id="error-username" class="errorbox"></div></td>
             </tr>
             <tr>
                 <td>Email</td>
                 <td>:</td>
-                <td><input class="inputtext-admin" type="text" name="email" value="<?php echo $this->session->userdata('email') ?>"/></td>
+                <td><input class="inputtext-setting" type="text" name="email" value="<?php echo $this->session->userdata('email') ?>"/></td>
                 <td><div id="error-email" class="errorbox"></div></td>
             </tr>
             <tr>
                 <td>Password</td>
                 <td>:</td>
-                <td><input class="inputtext-admin" type="password" name="password" value=""/></td>
+                <td><input class="inputtext-setting" type="password" name="password" value=""/></td>
                 <td><div id="error-password" class="errorbox"></div></td>
             </tr>
             <tr>
@@ -58,19 +58,19 @@
             <tr>
                 <td>Old Password</td>
                 <td>:</td>
-                <td><input class="inputtext-admin pass" type="password" name="old_pass" value=""/></td>
+                <td><input class="inputtext-setting pass" type="password" name="old_pass" value=""/></td>
                 <td><div id="error-old_pass" class="errorbox"></div></td>
             </tr>
             <tr>
                 <td>New Password</td>
                 <td>:</td>
-                <td><input class="inputtext-admin pass" type="password" name="new_pass" value=""/></td>
+                <td><input class="inputtext-setting pass" type="password" name="new_pass" value=""/></td>
                 <td><div id="error-new_pass" class="errorbox"></td>
             </tr>
             <tr>
                 <td>Re-New Password</td>
                 <td>:</td>
-                <td><input class="inputtext-admin pass" type="password" name="renew_pass" value=""/></td>
+                <td><input class="inputtext-setting pass" type="password" name="renew_pass" value=""/></td>
                 <td><div id="error-renew_pass" class="errorbox"></td>
             </tr>
             <tr>
