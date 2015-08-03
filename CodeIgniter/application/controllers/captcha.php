33ded65e4a5b5f6002d8c4f4882f749d04ab7c76
Fitri Nurjannah <widiw374@gmail.com>
2015-08-03 18:20:20 +0700

    remove unused

--- CodeIgniter/application/controllers/captcha.php
+++ CodeIgniter/application/controllers/captcha.php
@@ -2,22 +2,6 @@
 
 class Captcha extends CI_Controller {
     
-	function show_old()
-    {
-        $page = 'captcha_view';
-        if ( ! file_exists('../CodeIgniter/application/views/captcha/'.$page.'.php'))
-    	{
-    		// Whoops, we don't have a page for that!
-    		show_404();
-    	}
-    
-    	$data['title'] = ucfirst($page); // Capitalize the first letter
-    	
-    	//$this->load->view('templates/header', $data);
-    	$this->load->view('captcha/'.$page, $data);
-    	//$this->load->view('templates/footer', $data);
-    }
-    
     function frame()
     {
         $this->load->helper('html');
@@ -73,20 +57,6 @@ class Captcha extends CI_Controller {
     	$this->load->view('captcha/header', $data);
     	$this->load->view('captcha/'.$page, $data);
     	$this->load->view('captcha/footer', $data);
-    }
-    
-    function test_frame()
-    {
-        $page = 'iframe_captcha_testing';
-        if ( ! file_exists('../CodeIgniter/application/views/captcha/'.$page.'.php'))
-    	{
-    		// Whoops, we don't have a page for that!
-    		show_404();
-    	}
-    
-    	$data['title'] = ucfirst($page); // Capitalize the first letter
-    	
-    	$this->load->view('captcha/'.$page, $data);
     }
     
     public function _check_captcha()
